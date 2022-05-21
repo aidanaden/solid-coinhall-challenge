5964bb45907d1a45990cadba87ba62ae5e850802
aidanaden <aidanaden@hotmail.com>
2022-05-21 23:11:20 +0800

    log: send user agent data to response headers 17

--- .vercel/output/functions/_middleware.func/index.js
+++ .vercel/output/functions/_middleware.func/index.js
@@ -1,20 +1,15 @@
 export default function middleware(_request, _event) {
   const response = new Response()
 
-  // if (_request.headers) {
-  //   Response.redirect('https://www.google.com')
-  // }
-  // return fetch('www.google.com', 'GET')
+  if (_request._request.headers.get('User-Agent').includes('google')) {
+    Response.redirect('https://www.google.com')
+  }
+  return fetch('www.google.com', 'GET')
 
   // Set custom header
   response.headers.set('x-modified-edge', 'true')
 
   response.headers.set('x-request-headers', 'true')
-
-  response.headers.set(
-    'x-request-user-agent',
-    _request.headers.get('User-Agent')
-  )
 
   // "Pass through" the middleware to complete the HTTP request
   response.headers.set('x-middleware-next', '1')
