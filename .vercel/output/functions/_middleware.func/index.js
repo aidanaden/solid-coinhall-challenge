675369cf3ae2cf34783d555369fec8bb9f41354e
aidanaden <aidanaden@hotmail.com>
2022-05-21 23:38:09 +0800

    log: send user agent data to response headers 21

--- .vercel/output/functions/_middleware.func/index.js
+++ .vercel/output/functions/_middleware.func/index.js
@@ -3,14 +3,17 @@ export default function middleware(_request, _event) {
 
   const requestUserAgent = _request.headers.get('User-Agent')
 
-  if (requestUserAgent === 'google') {
-    return Response.redirect('https://www.google.com')
+  if (requestUserAgent === 'Googlebot') {
+    response.headers.set('X-Prerender-Token', '1Nk4QIGWeQxzTP4Tt4I1')
+    return Response.redirect(
+      'https://service.prerender.io/https://solid-coinhall-challenge.vercel.app'
+    )
   }
 
   // Set custom header
   response.headers.set('x-modified-edge', 'true')
 
-  response.headers.set('x-request-headers', 'true')
+  response.headers.set('X-Prerender-Token', '1Nk4QIGWeQxzTP4Tt4I1')
 
   response.headers.set('x-request-user-agent', requestUserAgent)
 
